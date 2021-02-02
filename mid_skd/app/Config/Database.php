2265caaa504d5dd657c8adc2fc2490698202c00e
M3118061 <62411823+M3118061@users.noreply.github.com>
2021-02-02 13:31:05 +0700

    Update Database.php

--- mid_skd/app/Config/Database.php
+++ mid_skd/app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'skd',
+		'database' => 'skd_uts',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
